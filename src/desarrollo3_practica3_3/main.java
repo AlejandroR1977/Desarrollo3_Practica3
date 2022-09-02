136f8c2a1f14ea1a66a075295ba6d8c4053b8a1a
AlejandroR1977 <111005527+AlejandroR1977@users.noreply.github.com>
2022-09-02 12:06:55 -0700

    Update main.java

--- src/desarrollo3_practica3_3/main.java
+++ src/desarrollo3_practica3_3/main.java
@@ -1,12 +1,20 @@
 package desarrollo3_practica3;
 
-public class NumeroNegativoExcepcion extends Exception {
-    
-    public NumeroNegativoExcepcion(String mensaje) {
-        super(mensaje);
-    }
-    public NumeroNegativoExcepcion(String mensaje,Throwable causa){
-        super(mensaje, causa);
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) throws NumeroNegativoExcepcion {
+        Scanner teclado = new Scanner(System.in);
+        double n;
+        System.out.println("Introduce un valor");
+        n = teclado.nextInt();
+        if(n>=0){
+            System.out.println("La raiz es: " + Math.sqrt(n));
+        }
+        else{
+            throw new NumeroNegativoExcepcion("El numero no es valido",
+            new Throwable("El numero es negativo"));
+        }
     }
 }
     
